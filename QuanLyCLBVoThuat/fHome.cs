d0db1209aaa46322fc7e76ff3b0775178e4dd226
DoanDinhChuong <chuongdd284@gmail.com>
2020-06-29 11:15:45 +0700

    desgin structure

--- QuanLyCLBVoThuat/fHome.cs
+++ QuanLyCLBVoThuat/fHome.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLyCLBVoThuat
 {
-    public partial class Form1 : Form
+    public partial class fHome : Form
     {
-        public Form1()
+        public fHome()
         {
             InitializeComponent();
         }
---
+++ QuanLyCLBVoThuat/fVoSinh.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCLBVoThuat
+{
+    public partial class fVoSinh : Form
+    {
+        public fVoSinh()
+        {
+            InitializeComponent();
+        }
+    }
+}
